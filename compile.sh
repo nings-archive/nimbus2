d61bb0f12e48f9da6febb93cb0941c9448081d12
ning <ningyuan.sg@gmail.com>
2017-05-06 16:01:51 +0800

    Add 'try again' to compile.sh, rename web_handler
    Adds 'try again' functionality to compile.sh in response to frequent
    build/install errors brought about by the use of Bash for Windows.
    
    <enter> must be pressed for compile.sh to run through build/install
    again. This is to prevent looping in cases where build/install fails
    due to errors with the code (rather than with Bash for Windows).
    
    Renames web_handler.go to generate_url.go, as there will likely be
    another file which actually handles http requests that will be named
    web_handler in the future.
    
    generate_url is a more descriptive name of what the code in that file
    does.

--- compile.sh
+++ compile.sh
@@ -1,7 +1,29 @@
-echo "- Removing old nimbus2..."
-rm ~/go/src/nimbus2/nimbus2
+declare install_status
+declare build_status
 
-sh ~/go/src/nimbus2/lib/install.sh
+compile () {
+    echo "- Removing old nimbus2..."
+    rm ~/go/src/nimbus2/nimbus2 2> /dev/null
+    remove_status=$?
+    if [ "$remove_status" = "1" ]; then
+        echo "File \"nimbus2\" not found"
+    fi
 
-echo "- Building nimbus2.go..."
-go build ~/go/src/nimbus2/nimbus2.go
+    sh ~/go/src/nimbus2/lib/install.sh
+    install_status=$?
+
+    echo "- Building nimbus2.go..."
+    go build ~/go/src/nimbus2/nimbus2.go
+    build_status=$?
+}
+
+while true; do
+    compile
+    if [ "$install_status" == "0" ] && [ "$build_status" == "0" ]; then
+        echo "- Success!"
+        break
+    fi
+    echo "- There was an error in the install or build stages"
+    echo "- Try again: <enter>; Abort: <ctrl-c>"
+    read text
+done
